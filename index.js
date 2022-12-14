fec842ae5c6963842e8bb8626e3f16f1b3eb5869
Cody Cooper <cody_codster@live.com>
2022-12-14 13:23:37 -0600

    Updated and debugged index.js

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ const teamArr = []
 
 // Questions for User
 const questionObj ={
-    nameInputQ: {
+    managerNameInputQ: {
         type: 'input',
         name: 'name',
         message: 'Please enter the name of your team manager -> ',
+        validate: userInput => { //Requires a valid employee name
+            if (userInput && isNaN(userInput)) {
+            return true;
+            } else {
+                console.warn('\x1b[31m', '\nPlease provide a valid name!')
+            }
+        }
+    },
+    nameInputQ: {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the name of your new team member -> ',
         validate: userInput => { //Requires a valid employee name
             if (userInput && isNaN(userInput)) {
             return true;
@@ -26,7 +38,7 @@ const questionObj ={
     emailInputQ: {
         type: 'input',
         name: 'email',
-        message: 'Please enter the manager\'s email address -> ',
+        message: 'Please provide the team member\'s email address -> ',
         validate: userInput => {
             if (userInput && userInput.includes('@') && userInput.includes('.com')) {
                 return true
@@ -39,7 +51,7 @@ const questionObj ={
     idNumberQ: {
         type: 'input',
         name: 'id',
-        message: 'Please enter the manager\'s ID number -> ',
+        message: 'Please provide a team member ID -> ',
         validate: userInput => { //Requires valid employee ID
             if (userInput && !isNaN(userInput)) {
                 return true;
@@ -47,13 +59,6 @@ const questionObj ={
                 console.warn('\x1b[31m', '\nPlease provide a valid ID number!')
             }
         }
-    },
-
-    confirmQ: {
-        type: 'list',
-        name: 'addEmployee',
-        message: 'Please specify an additional team member to add.  -> ',
-        choices: ['Intern', 'Engineer', 'I am finished']
     },
 
     officeNumQ: {
@@ -80,27 +85,111 @@ const questionObj ={
                 console.warn('\x1b[31m', '\nPlease provide a valid Github username!')
             }
         }  
-        } 
-}
+    },
+    
+    schoolQ: {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter the name of school the intern attended -> ',
+        validate: userInput => { //Requires a valid school name
+            if(userInput && isNaN(userInput)) {
+                return true;
+            } else {
+                console.warn('\x1b[31m', '\nPlease provide a valid school name!')
+            }
+        }
+    },
 
+    githubQ: {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter the engineer\'s Github username -> ',
+        validate: userInput => { //Requires a valid school name
+            if(userInput && isNaN(userInput)) {
+                return true;
+            } else {
+                console.warn('\x1b[31m', '\nPlease provide a valid Github username!')
+            }
+        }
+    },
+
+    confirmQ: {
+        type: 'list',
+        name: 'addEmployee',
+        message: 'Please specify an additional team member to add.  -> ',
+        choices: ['Intern', 'Engineer', 'I am finished']
+    }
+}
 // Destructed questionObj for easier access to questions
-const {nameInputQ, idNumberQ,  emailInputQ,  officeNumQ, confirmQ} = questionObj;
+const {managerNameInputQ, nameInputQ, idNumberQ,  emailInputQ,  officeNumQ, confirmQ, schoolQ, githubQ} = questionObj;
 
 // Adding Manager to start team build
 function start() {
-    inquirer.prompt([ nameInputQ, idNumberQ,  emailInputQ,  officeNumQ, confirmQ ])
+    inquirer.prompt([managerNameInputQ, idNumberQ,  emailInputQ,  officeNumQ, confirmQ ])
     .then((data) => {
         const manager = new Manager(data.name, data.id, data.email, data.officeNumber) // Creates new Manager object
         teamArr.push(manager); //Pushes team member onto array
 
         switch(data.addEmployee) { // Checks if user wants to add another team member
             case 'Intern': addIntern();
+            break;
             case 'Engineer': addEngineer();
+            break;
             case 'I am finished': generateHTML();
+            break;
         }
+    })
+    .catch((err) => {
+        console.log(err)
     })
 }
 
+function addIntern() {
+    inquirer.prompt([nameInputQ, idNumberQ, emailInputQ, schoolQ, confirmQ])
+    .then((data) => {
+        let newIntern = new Intern(data.name, data.id, data.email, data.school);
+        teamArr.push(newIntern);
+
+        switch(data.addEmployee) { // Checks if user wants to add another team member
+            case 'Intern': addIntern();
+            break;
+            case 'Engineer': addEngineer();
+            break;
+            case 'I am finished': generateHTML();
+            break;
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+function addEngineer() {
+    inquirer.prompt([nameInputQ, idNumberQ, emailInputQ, githubQ, confirmQ])
+        .then((data) => {
+            let newEngineer = new Engineer(data.name, data.id, data.email, data.github);
+            teamArr.push(newEngineer)
+
+            switch(data.addEmployee) { // Checks if user wants to add another team member
+                case 'Intern': addIntern();
+                break;
+                case 'Engineer': addEngineer();
+                break;
+                case 'I am finished': generateHTML();
+                break;
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        
+}
+
+function generateHTML() {
+    console.log(typeof teamArr[0])
+}
+
+// Starts the question prompts
 start()
 
 
