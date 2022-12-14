59153272a0287d08377e21a2d085838be2bdc3ab
Cody Cooper <cody_codster@live.com>
2022-12-14 17:24:40 -0600

    Added tests to intern.test.js file

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,1 +1,25 @@
 const Intern = require('../lib/intern');
+
+describe('Intern class', () => {
+
+    describe('new Intern class', () => {
+        it('should create a new Intern employee object', () => {
+            const newObj = new Intern();
+        expect(typeof newObj).toBe('object');
+        });
+    });
+
+    describe('getSchool', () => {
+        it('should return a school name using the getSchool method', () => {
+            const intern = new Intern('','','','University');
+        expect(intern.getSchool()).toBe('University');
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return a role property from the intern object', () => {
+            const intern = new Intern();
+        expect(intern.getRole()).toBe('Intern');
+        });
+    });
+});
