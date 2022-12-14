2611dcbd4704d3f6d0e1e370f59d546b32f3c05b
Cody Cooper <cody_codster@live.com>
2022-12-14 17:28:20 -0600

    Added tests to manager.test.js file

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,18 @@
 const Manager = require('../lib/manager');
+
+describe('Manager class', () => {
+
+    describe('new Manager class', () => {
+        it('should create a new manager object', () => {
+            const newObj = new Manager();
+        expect(typeof newObj).toBe('object');
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return the role property of the manager object', () => {
+            const manager = new Manager();
+        expect(manager.getRole()).toBe('Manager');
+        });
+    });
+});
